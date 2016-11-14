6d7a94aad4be4f655c2c740f86c39386a424226d
Copperfield <xwhfcenter@gmail.com>
2016-11-14 17:59:09 +0800

    修改了21章和22章的目录格式

---
+++ program/ch21/Program21_7/WideCardDemo2.java
@@ -0,0 +1,16 @@
+public class WideCardDemo2 {
+  public static void main(String[] args) {
+    GenericStack<Integer> intStack = new GenericStack<Integer>();
+    intStack.push(1);
+    intStack.push(2);
+    intStack.push(-2);
+
+    print(intStack);
+  }
+
+  public static void print(GenericStack<?> stack) {
+    while (!stack.isEmpty()) {
+      System.out.print(stack.pop() + " ");
+    }
+  }
+}
