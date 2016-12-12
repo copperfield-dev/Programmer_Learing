40d216bb26577e3bdeb65e7dc5f404a24d588a67
Copperfield <xwhfcenter@gmail.com>
2016-12-12 12:20:56 +0800

    提交了30.7，30.8，30.9的代码

---
+++ ch30/program/Program30_7&8&9/StudentAddress.java
@@ -0,0 +1,40 @@
+import java.io.Serializable;
+
+/**
+ * Created by copperfield on 12/12/2016.
+ */
+public class StudentAddress implements Serializable {
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    
+    public StudentAddress(String name, String street, String city, String state, String zip) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getStreet() {
+        return street;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+    
+    public String getState() {
+        return state;
+    }
+    
+    public String getZip() {
+        return zip;
+    }
+}
