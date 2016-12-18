2f5f9d7ef3334aaa9fe4fe66a998f815bce8fd64
Copperfield <xwhfcenter@gmail.com>
2016-12-18 22:57:06 +0800

    实现了一个小的世界时钟

---
+++ ch31/program/Program31_1&2&3/WorldClockApp.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by copperfield on 18/12/2016.
+ */
+public class WorldClockApp extends JApplet {
+    public WorldClockApp() {
+        add(new WorldClockControl());
+    }
+}
