cb11c2b2a7d1296a309351b5148eca5c6959abeb
copperfield-dev <xwhfcenter@gmail.com>
2024-11-12 11:16:58 +0800

    更新学习代码

--- Python/Python Crash Course/python_work/pizza.py
+++ Python/Python Crash Course/python_work/pizza.py
@@ -10,3 +10,12 @@ print(f"You ordered a {pizza['crust']}-crust pizza "
 
 for topping in pizza['toppings']:      
     print(f"\t{topping}")
+
+def make_pizza(size, *toppings):     
+    """概述要制作的比萨"""     
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")     
+    for topping in toppings:         
+        print(f"- {topping}")  
+    
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
