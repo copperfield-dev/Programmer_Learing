9a25bc2f2587b9296fb4023fc8e9c0491f7bf5e8
copperfield-dev <xwhfcenter@gmail.com>
2024-11-13 16:41:19 +0800

    完成外星人的移动

--- Python/Python Crash Course/alien_invasion/alien_invasion.py
+++ Python/Python Crash Course/alien_invasion/alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()            
             self.clock.tick(60)
 
@@ -44,6 +45,11 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """检查是否有外星人位于屏幕边缘, 并更新整个外星舰队的位置"""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _check_events(self):
         """响应按键和鼠标事件"""
@@ -72,6 +78,20 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _check_fleet_edges(self):           
+        """在有外星人到达边缘时采取相应的措施"""
+        for alien in self.aliens.sprites():               
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):           
+        """将整个外星舰队向下移动,并改变它们的方向"""           
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+
+        self.settings.fleet_direction *= -1
 
     def _create_fleet(self):
         """创建一个外星舰队"""         
